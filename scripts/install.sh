86c4585f786769bf45039ef9f830e0ae3d06dd95
Hezekiah Michael <spiritomb@protonmail.com>
2026-07-31 18:22:02 -0400

    install docker packages from docker's repo, not debian's

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,7 @@ sudo apt-get install -y python3 python3-pip python3-dev libffi-dev libssl-dev
 
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker ${USER}
-# sudo pip3 install docker-compose
+sudo apt-get update
+sudo apt-get install -y docker-compose-plugin docker-buildx-plugin
 sudo systemctl enable docker
 newgrp docker
